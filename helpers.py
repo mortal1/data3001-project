1d622fd709858fd3c82265bcb1b8b54c6ee75345
Lake <laeequejamdar@gmail.com>
2022-11-01 22:32:12 +1100

    moved saving a colouring to helpers.py

--- helpers.py
+++ helpers.py
@@ -1,4 +1,6 @@
 import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
 
 def vectorise(x):
     return np.array(x).reshape((-1, 1))
@@ -8,3 +10,13 @@ def diff_matrix(A):
     M = np.outer(np.ones_like(A), A)
     return M - M.T
 
+
+def save_colouring(A, filename):
+    IOTA = np.arange(len(A))
+    scores = MinMaxScaler().fit_transform(vectorise(A)).reshape((-1))
+
+    df = pd.DataFrame()
+    df['index'] = IOTA+1
+    # df['score'] = [int(i*100) for i in scores]
+    df['score'] = np.around(scores*100)
+    df.to_csv(filename, sep='\t', header=False, index=False)
